42a54553ed1248eff1f8dba722e8a7e8ae8eef49
wenzli0510 <wenzhe.li@uzh.ch>
2023-04-23 16:12:11 +0200

    complete CommentServiceTest

---
+++ src/test/java/ASESpaghettiCode/PostServer/Service/CommentServiceTest.java
@@ -0,0 +1,100 @@
+package ASESpaghettiCode.PostServer.Service;
+
+import ASESpaghettiCode.PostServer.Model.Comment;
+import ASESpaghettiCode.PostServer.Model.CommentPostDTO;
+import ASESpaghettiCode.PostServer.Model.Post;
+import ASESpaghettiCode.PostServer.Repository.CommentRepository;
+import ASESpaghettiCode.PostServer.Repository.PostRepository;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class CommentServiceTest {
+    private final CommentRepository commentRepository = mock(CommentRepository.class);
+    private final PostRepository postRepository = mock(PostRepository.class);
+
+    private final CommentService commentService = new CommentService(commentRepository, postRepository);
+    Post post = new Post("authorId", "content");
+    Comment comment = new Comment("commentAuthorId", "commentAuthorName", "commentAuthorName", "targetPostId", "commentText");
+
+    @Test
+    void findCommentsByPostIdTest_Success() {
+        post.setComments(List.of(comment));
+        when(postRepository.findById(any(String.class))).thenReturn(Optional.ofNullable(post));
+
+        assertEquals(List.of(comment), commentService.findCommentsByPostId("1"));
+    }
+
+    @Test
+    void findCommentsByPostIdTest_Fail() {
+        when(postRepository.findById(any(String.class))).thenReturn(Optional.empty());
+
+        assertThrows(RuntimeException.class, () -> commentService.findCommentsByPostId("1"));
+    }
+
+    @Test
+    void deleteCommentTest_Success() {
+        List<Comment> initialComments = new ArrayList<>();
+        post.setComments(initialComments);
+        post.addComments(comment);
+        when(commentRepository.findById(any(String.class))).thenReturn(Optional.ofNullable(comment));
+        when(postRepository.findById(any(String.class))).thenReturn(Optional.ofNullable(post));
+
+        commentService.deleteComment("1", "commentAuthorId");
+
+        verify(postRepository, times(1)).save(any(Post.class));
+        verify(commentRepository, times(1)).delete(any(Comment.class));
+    }
+
+    @Test
+    void deleteCommentTest_NoComment() {
+        when(commentRepository.findById(any(String.class))).thenReturn(Optional.empty());
+
+        assertThrows(RuntimeException.class, () -> commentService.deleteComment("1", "1"));
+    }
+
+    @Test
+    void deleteCommentTest_Unauthorized() {
+        when(commentRepository.findById(any(String.class))).thenReturn(Optional.ofNullable(comment));
+
+        assertThrows(RuntimeException.class, () -> commentService.deleteComment("1", "1"));
+    }
+
+    @Test
+    void deleteCommentTest_NoPost() {
+        when(commentRepository.findById(any(String.class))).thenReturn(Optional.ofNullable(comment));
+        when(postRepository.findById(any(String.class))).thenReturn(Optional.empty());
+
+        assertThrows(RuntimeException.class, () -> commentService.deleteComment("1", "commentAuthorId"));
+    }
+
+    @Test
+    void updateCommentTest_Success() {
+        CommentPostDTO commentPostDTO = new CommentPostDTO();
+        when(commentRepository.findById(any(String.class))).thenReturn(Optional.ofNullable(comment));
+
+        commentService.updateComment("commentAuthorId", "1", commentPostDTO);
+
+        verify(commentRepository, times(1)).save(any(Comment.class));
+    }
+
+    @Test
+    void updateCommentTest_NoComment() {
+        when(commentRepository.findById(any(String.class))).thenReturn(Optional.empty());
+
+        assertThrows(RuntimeException.class, () -> commentService.updateComment("1", "1", new CommentPostDTO()));
+    }
+
+    @Test
+    void updateCommentTest_Unauthorized() {
+        when(commentRepository.findById(any(String.class))).thenReturn(Optional.ofNullable(comment));
+
+        assertThrows(RuntimeException.class, () -> commentService.updateComment("1", "1", new CommentPostDTO()));
+    }
+}
